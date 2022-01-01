6314858145e265833576be2b8831ea34eb45390d
Bastian Schmidt <batzen@gmx.org>
2022-01-01 22:41:53 +0100

    Improving sample content

--- src/XAMLTools.WPFApp/AssemblyInfo.cs
+++ src/XAMLTools.WPFApp/AssemblyInfo.cs
@@ -8,4 +8,3 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-
