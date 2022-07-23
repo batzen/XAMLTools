2bc8be0a9146b9b38ac7644c4a3f447f7679f232
Bastian Schmidt <batzen@gmx.org>
2022-07-23 20:26:15 +0200

    Adding test case for #52

---
+++ src/tests/XAMLTools.WPFApp/Controls/MyControl1.cs
@@ -0,0 +1,8 @@
+// ReSharper disable once CheckNamespace
+namespace XAMLTools.WPFApp.Controls.MyControl1Namespace;
+
+using System.Windows.Controls;
+
+public class MyControl1 : Control
+{
+}
---
+++ src/tests/XAMLTools.WPFApp/Controls/MyControl2.cs
@@ -0,0 +1,8 @@
+// ReSharper disable once CheckNamespace
+namespace XAMLTools.WPFApp.Controls.MyControl2Namespace;
+
+using System.Windows.Controls;
+
+public class MyControl2 : Control
+{
+}
